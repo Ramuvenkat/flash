2135c117c669504a6afafc3284461912f95cdcf0
venka <venkateswarluvcs@gmail.com>
2023-11-10 22:57:56 +0530

    venkat commit

--- MyExample/src/My/Test.java
+++ MyExample/src/My/Test.java
@@ -5,7 +5,10 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("welcome to java");
-
+Test t=new Test();
+t.m1();
 	}
-
+public void m1() {
+	System.out.println("m1 method call ");
+}
 }
