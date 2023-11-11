ac95a78d83c0c704a0c37676ebdf20b57e3a0d74
venka <venkateswarluvcs@gmail.com>
2023-11-11 10:09:45 +0530

    javacode commit

---
+++ MyExample/src/My/Venkat.java
@@ -0,0 +1,18 @@
+package My;
+
+import java.util.TreeMap;
+
+public class Venkat {
+	public static void main(String[] args) {
+		TreeMap a=new TreeMap();
+		a.put(108, "Ramu");
+		a.put(101, "Venky");
+		a.put(109, "Srinu");
+		a.put(105, "Laxman");
+		a.put(106, null);
+		a.put(106,null);
+		
+		System.out.println(a);
+	}
+
+}
